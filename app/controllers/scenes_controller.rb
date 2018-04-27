98716114567ea3f664bf3f42fb05de785194b205
Viktharien <viktharien@zoho.com>
2018-04-27 20:26:00 +0000

    create other controllers with actions

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -21,7 +21,11 @@ class ScenesController < ApplicationController
     end
     
     def update
-        if @scene.update(scene_params)    
+        if @scene.update(scene_params)
+            
+        else
+            
+        end
     end
     
     
