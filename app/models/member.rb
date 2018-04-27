159c11ab0c4756d5b41e70845a99f6b183302beb
Viktharien <viktharien@zoho.com>
2018-04-27 21:03:10 +0000

    set up validations and routes

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,5 @@
 class Member < ApplicationRecord
     has_secure_password
     has_many :posts, dependent: :destroy
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
