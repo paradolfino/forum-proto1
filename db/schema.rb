7dde13d960e82f50a61fde9d5955be6ea2cbf865
Viktharien <viktharien@zoho.com>
2018-04-27 16:53:56 +0000

    create post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_27_165106) do
+ActiveRecord::Schema.define(version: 2018_04_27_165304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2018_04_27_165106) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "member_id"
+    t.bigint "scene_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_posts_on_member_id"
+    t.index ["scene_id"], name: "index_posts_on_scene_id"
+  end
+
   create_table "scenes", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -30,4 +41,6 @@ ActiveRecord::Schema.define(version: 2018_04_27_165106) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "members"
+  add_foreign_key "posts", "scenes"
 end
