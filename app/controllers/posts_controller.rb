20831b4016e5123c8ef0bb4c83229f00d9f39569
Viktharien <viktharien@zoho.com>
2018-04-27 20:20:38 +0000

    create other controllers with actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
     private
     
         def post_params
-           
+           params.require(:post).permit(:title, :content)
         end
         
         def set_post
