8db421bbe312c92e5af2c71ceff0bffa2603abc3
Viktharien <viktharien@zoho.com>
2018-04-27 20:19:03 +0000

    create other controllers with actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
        if @post.update(post_params)
           redirect_to @post 
        else
-           
+           render 'edit'
        end
     end
     
