ac70af172c77d75db605afbfc9e51a984b3d5ca1
Viktharien <viktharien@zoho.com>
2018-04-27 20:10:14 +0000

    create other controllers with actions

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -11,6 +11,10 @@ class MembersController < ApplicationController
     def new
        @member = Member.new 
     end
+    
+    def create
+       @member = Member.new(member_params) 
+    end
         
     
 end
