43f1829535d2f0cf3500a8ad99b2197ad6933a5e
Viktharien <viktharien@zoho.com>
2018-04-27 21:01:07 +0000

    set up validations and routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :member
   belongs_to :scene
   validates :title, presence: true, length: { minimum: 2 }
+  validates :content, presence: true, length: { minimum: 2 }
 end
