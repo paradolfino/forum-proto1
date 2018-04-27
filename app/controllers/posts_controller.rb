6d32ef44b5ed05a5ed289a9c6461d507a6c0209f
Viktharien <viktharien@zoho.com>
2018-04-27 20:48:09 +0000

    add session related methods to application controller and before_action to other controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
     
     before_action :set_post, only: [:show, :edit, :update, :destroy]
+    before_action :require_member, only: [:new, :edit, :destroy]
     
     def index
        @posts = Post.all 
