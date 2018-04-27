f5918309dd3d1197ffcb64f5f2b717973ed5b48a
Viktharien <viktharien@zoho.com>
2018-04-27 21:06:20 +0000

    set up validations and routes

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,5 @@ class Member < ApplicationRecord
     has_many :posts, dependent: :destroy
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
     validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
 end
