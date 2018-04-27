6498e0df579cecfbf7428d5911296097c6066b2f
Viktharien <viktharien@zoho.com>
2018-04-27 20:35:22 +0000

    add session related methods to application controller and before_action to other controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user 
+
+    def current_user 
+        @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+    end
+  
+    def require_user 
+  	   redirect_to '/login' unless current_user 
+    end
+	
+    def require_same_user(user)
+	    if current_user != user
+    	   flash[:notice] = "You don't have the right to view this!"
+    	   redirect_to '/'
+	    end
+    end
 end
