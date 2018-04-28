a833dc9f641e0c102f0e63cf2b2a2cf8361f57dc
Viktharien <viktharien@zoho.com>
2018-04-28 21:07:53 +0000

    set up roles controller with actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'roles/new'
+  get 'roles/create'
+  get 'roles/edit'
+  get 'roles/update'
+  get 'roles/destroy'
   resources :scenes do
      resources :posts 
   end
