ccc683aef9a5e869fd2955bac489826f097d1e03
Viktharien <viktharien@zoho.com>
2018-04-28 21:22:03 +0000

    set up require_admin method in application controller

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,5 +4,5 @@ class Member < ApplicationRecord
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
     validates :email, presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
-    validates :password_digest, presence: true, length: { minimum: 8 }
+    validates :password, presence: true, length: { minimum: 8 }
 end
