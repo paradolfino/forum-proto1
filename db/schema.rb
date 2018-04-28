b2cc2e425bcdff67fcc2664b0e6a1729005ce491
Viktharien <viktharien@zoho.com>
2018-04-28 20:55:44 +0000

    set up roles for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_27_165304) do
+ActiveRecord::Schema.define(version: 2018_04_28_205437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2018_04_27_165304) do
     t.datetime "updated_at", null: false
     t.index ["member_id"], name: "index_posts_on_member_id"
     t.index ["scene_id"], name: "index_posts_on_scene_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "scenes", force: :cascade do |t|
