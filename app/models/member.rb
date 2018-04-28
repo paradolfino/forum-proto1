4f398b6e1631b28b1f41ac68c67a3e277b0b4be7
Viktharien <viktharien@zoho.com>
2018-04-28 21:28:02 +0000

    set up require_admin method in application controller

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,4 +5,8 @@ class Member < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
     validates :password, presence: true, length: { minimum: 8 }
+    
+    def set_role
+        
+    end
 end
