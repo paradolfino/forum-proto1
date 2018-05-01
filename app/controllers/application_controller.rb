a802cf0cc2672e49d14fb458487626ec75c50b09
Viktharien <viktharien@zoho.com>
2018-05-01 14:28:51 +0000

    implement require_admin method in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,6 @@ class ApplicationController < ActionController::Base
     end
     
     def require_admin
-        redirect_to '/login' unless current_member.role_id = Role.find_by(title: "Admin").id
+        redirect_to '/login' unless current_member && current_member.role_id = Role.find_by(title: "Admin").id
     end
 end
