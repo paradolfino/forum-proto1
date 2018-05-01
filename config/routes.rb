9bbf8482bf466d9ed7012fedca5a18243e2c460a
Viktharien <viktharien@zoho.com>
2018-05-01 14:32:37 +0000

    implement require_admin method in controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  
+  get '/signup' => 'members#new'
+  post '/signup' => 'members#create'
+  resources :members
+  resources :roles
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   resources :scenes do
      resources :posts 
   end
-  resources :members
-  resources :roles
+  
 end
