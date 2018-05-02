e4cde2c58fac47c936ba98e125e67d6f2f454ae8
Viktharien <viktharien@zoho.com>
2018-05-02 16:43:46 +0000

    test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
     #before_action :require_member, only: [:new, :edit, :destroy]
     
     def show
-        @comments = @post.comments
+        
     end
     
     def new
